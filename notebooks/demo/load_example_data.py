098f4d57f670c9388407aa0f220bc9718045127b
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-05-20 10:37:35 +0100

    update outputs

--- notebooks/demo/load_example_data.py
+++ notebooks/demo/load_example_data.py
@@ -2,7 +2,7 @@ import polars as pl
 import rainfallqc
 
 def load_local_gdsr_data(gauge_id):
-    data_path = f"../data/GDSR/{gauge_id}.txt"
+    data_path = f"../../data/GDSR/{gauge_id}.txt"
     # read in metadata of gauge
     gdsr_metadata = rainfallqc.utils.data_readers.read_gdsr_metadata(data_path)
     rain_col = f"rain_{gdsr_metadata['original_units']}"
@@ -19,13 +19,13 @@ def load_local_gdsr_data(gauge_id):
         gdsr_data, gdsr_metadata, multiplying_factor=24
     )
     gdsr_data = rainfallqc.utils.data_utils.replace_missing_vals_with_nan(
-        gdsr_data, rain_col=rain_col, missing_val=int(gdsr_metadata["no_data_value"])
+        gdsr_data, rain_col=rain_col, missing_val=int(gdSsr_metadata["no_data_value"])
     )
     return gdsr_data.select(["time", rain_col])
 
 
 def load_local_gdsr_metadata(gauge_id):
-    data_path = f"../data/GDSR/{gauge_id}.txt"
+    data_path = f"../../data/GDSR/{gauge_id}.txt"
     # read in metadata of gauge
     gdsr_metadata = rainfallqc.utils.data_readers.read_gdsr_metadata(data_path)
     return gdsr_metadata
