8c10666bc6adf2d67e4b6049753402fbabe21a15
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-05-21 13:48:23 +0100

    update

--- notebooks/demo/run_qc_framework.py
+++ notebooks/demo/run_qc_framework.py
@@ -2,22 +2,22 @@ import inspect
 from rainfallqc import gauge_checks, comparison_checks, timeseries_checks, neighbourhood_checks
 
 ALL_QC_METHODS = {
-    "QC1": {"function": gauge_checks.get_years_where_nth_percentile_is_zero},
-    "QC2": {"function": gauge_checks.get_years_where_annual_mean_k_top_rows_are_zero},
+    "QC1": {"function": gauge_checks.check_years_where_nth_percentile_is_zero},
+    "QC2": {"function": gauge_checks.check_years_where_annual_mean_k_top_rows_are_zero},
     "QC3": {"function": gauge_checks.check_temporal_bias},
     "QC4": {"function": gauge_checks.check_temporal_bias},
-    "QC5": {"function": gauge_checks.intermittency_check},
-    "QC6": {"function": gauge_checks.breakpoints_check},
-    "QC7": {"function": gauge_checks.min_val_change},
+    "QC5": {"function": gauge_checks.check_intermittency},
+    "QC6": {"function": gauge_checks.check_breakpoints},
+    "QC7": {"function": gauge_checks.check_min_val_change},
     "QC8": {"function": comparison_checks.check_annual_exceedance_etccdi_r99p},
     "QC9": {"function": comparison_checks.check_annual_exceedance_etccdi_prcptot},
     "QC10": {"function": comparison_checks.check_exceedance_of_rainfall_world_record},
     "QC11": {"function": comparison_checks.check_annual_exceedance_etccdi_rx1day},
-    "QC12": {"function": timeseries_checks.dry_period_cdd_check},
-    "QC13": {"function": timeseries_checks.daily_accumulations},
-    "QC14": {"function": timeseries_checks.monthly_accumulations},
-    "QC15": {"function": timeseries_checks.streaks_check},
-    "QC16": {"function": neighbourhood_checks.wet_neighbour_check},
+    "QC12": {"function": timeseries_checks.check_dry_period_cdd},
+    "QC13": {"function": timeseries_checks.check_daily_accumulations},
+    "QC14": {"function": timeseries_checks.check_monthly_accumulations},
+    "QC15": {"function": timeseries_checks.check_streaks},
+    "QC16": {"function": neighbourhood_checks.check_wet_neighbours},
  }
 
 
